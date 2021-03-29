62b8d1f5013d41683d2ad94720d1afaf2a805197
oduran <oduran@dynasoft.hn>
2021-03-29 15:37:17 -0600

    Agregando socket.io

--- backendHabits/controllers/medicineController.js
+++ backendHabits/controllers/medicineController.js
@@ -5,7 +5,8 @@ const { validationResult } = require('express-validator');
 const moment = require('moment');
 /* POST */
 exports.createMedicine = (req, res) => {
-
+    let io = req.app.get('socketio');
+    
     // Revisamos si hay errores
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -13,11 +14,11 @@ exports.createMedicine = (req, res) => {
     }
 
     try {
-        req.body.id=uuidv4();
+        req.body.id = uuidv4();
         req.body.registerDate = moment(new Date()).format('DD/MM/YYYY');
         medicines.push(req.body);
-        
-        res.send(medicines);
+        io.sockets.emit('Data created', req.body);
+        res.json({ msg: "Se creó el medicamento correctamente." });
     } catch (error) {
         console.log(error);
         res.status(400).send('Hubo un error');
@@ -26,10 +27,10 @@ exports.createMedicine = (req, res) => {
 }
 
 /* GET */
-exports.getMedicines = (req,res)=>{
+exports.getMedicines = (req, res) => {
     try {
         const resp = medicines;
-        res.json({medicines:resp});
+        res.json({ medicines: resp });
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error');
@@ -37,11 +38,10 @@ exports.getMedicines = (req,res)=>{
 }
 
 /* GET /:id */
-exports.getMedicine = (req,res)=>{
+exports.getMedicine = (req, res) => {
     try {
-        const resp = medicines.filter(medicine=> medicine.id===req.params.id);
-        console.log(resp);
-        res.json({medicines:resp});
+        const resp = medicines.filter(medicine => medicine.id === req.params.id);
+        res.json({ medicines: resp });
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error');
@@ -49,19 +49,19 @@ exports.getMedicine = (req,res)=>{
 }
 
 /* PUT /:id */
-exports.updateMedicine = (req,res)=>{
+exports.updateMedicine = (req, res) => {
     try {
-        medicines.filter((medicine,index)=> {
-            if(medicine.id===req.params.id){
-                medicines[index].name = req.body.name?req.body.name:medicines[index].name;
-                medicines[index].type = req.body.type?req.body.type:medicines[index].type;
-                medicines[index].quantity = req.body.quantity?req.body.quantity:medicines[index].quantity;
-                medicines[index].price = req.body.price?req.body.price:medicines[index].price;
-                medicines[index].location = req.body.location?req.body.location:medicines[index].location;
-                res.json({medicine:medicines[index]});
+        medicines.filter((medicine, index) => {
+            if (medicine.id === req.params.id) {
+                medicines[index].name = req.body.name ? req.body.name : medicines[index].name;
+                medicines[index].type = req.body.type ? req.body.type : medicines[index].type;
+                medicines[index].quantity = req.body.quantity ? req.body.quantity : medicines[index].quantity;
+                medicines[index].price = req.body.price ? req.body.price : medicines[index].price;
+                medicines[index].location = req.body.location ? req.body.location : medicines[index].location;
+                res.json({ msg: "Se actualizó el medicamento correctamente." });
             }
         });
-        
+
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error');
@@ -69,15 +69,17 @@ exports.updateMedicine = (req,res)=>{
 }
 
 /* DELETE /:id */
-exports.deleteMedicine = (req,res)=>{
+exports.deleteMedicine = (req, res) => {
+    let io = req.app.get('socketio');
     try {
-        medicines.filter((medicine,index)=> {
-            if(medicine.id===req.params.id){
-                medicines.splice(index,1);
-                res.json({medicine:medicines});
+        medicines.filter((medicine, index) => {
+            if (medicine.id === req.params.id) {
+                io.sockets.emit('Deleted data', medicines[index]);
+                medicines.splice(index, 1);
+                res.json({ msg: "Se eliminó el medicamento correctamente." });
             }
         });
-        
+
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error');
--- backendHabits/index.js
+++ backendHabits/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
-
 // Creando servidor
 const app = express();
-
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const medicines = require('./routes/medicines');
 // Habilitamos express.json
 app.use(express.json({ extended: true }));
 
+// Improtando rutas
+app.use('/api/medicines', medicines);
+app.set('socketio',io);
 const PORT = process.env.PORT || 3000;
 
-// Improtando rutas
-app.use('/api/medicines', require('./routes/medicines'));
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`El servidor se levanto en el puerto ${PORT}`);
 });
